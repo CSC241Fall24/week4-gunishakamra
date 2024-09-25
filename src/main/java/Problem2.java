8fdd4d1c61291df2ce1fafbc9dca14f0df2add19
gunishakamra <gk989980@wcupa.edu>
2024-09-25 15:56:39 -0400

    fixed

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -7,6 +7,7 @@ public class Problem2 {
        
         ListNode newNode = new ListNode(val);
 
+        // If the list is empty or position is 1, insert at the beginning
         if (position == 1 || head == null) {
             newNode.next = head;
             return newNode;
@@ -15,31 +16,33 @@ public class Problem2 {
         ListNode current = head;
         int currentIndex = 1;
 
-        while (current != null && currentIndex < position - 1) {
+        // Traverse the list to find the position just before the insertion point
+        while (current.next != null && currentIndex < position - 1) {
             current = current.next;
             currentIndex++;
         }
 
-        if (current == null) {
-            return head;  
+        // If we reached the end of the list or the position is out of bounds, append the node at the end
+        if (current.next == null && currentIndex < position - 1) {
+            current.next = newNode;
+        } else {
+            newNode.next = current.next;
+            current.next = newNode;
         }
-
-        newNode.next = current.next;
-        current.next = newNode;
 
         return head;
     }
-    public static void main(String [] args){
-        ListNode l1 = new ListNode(1);
-            l1.next = new ListNode(2);
-            l1.next.next = new ListNode(3);
-    
-          
-            // Inserts elements in a list at a particular position
-            ListNode insertedList = insert(l1, 4, 2);
-    
-            // Print the inserted list
-            Problem1.printList(insertedList);
 
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+
+        // Inserts element at the end since the position is out of bounds
+        ListNode insertedList = insert(l1, 5, 10);
+
+        // Print the inserted list
+        Problem1.printList(insertedList);  
     }
 }
