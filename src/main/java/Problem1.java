c905408e960c805fcab93773b6fa7326ee61cb3c
gunishakamra <gk989980@wcupa.edu>
2024-09-25 15:12:03 -0400

    done problem 1

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,41 +1,37 @@
 public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return copy(l2);
+            if (l1 == null) {
+                return l2;
+            }
+            ListNode current = l1;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = l2;
+            return l1;
         }
-
-        ListNode current = l1;
-        while (current.next != null) {
-            current = current.next;
+        
+        public static void printList(ListNode current) {
+            while (current != null) {
+                System.out.print(current.val + " ");
+                current = current.next;
+            }
+            System.out.println();
         }
-
-        ListNode l2Current = l2;
-        while (l2Current != null) {
-            current.next = new ListNode(l2Current.val); 
-            current = current.next; 
-            l2Current = l2Current.next; 
-        }
-
-        return l1; 
-    }
-
-    private static ListNode copy(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-
-        ListNode newHead = new ListNode(head.val);
-        ListNode current = newHead;
-        ListNode originalCurrent = head.next;
-
-        // Iterate through the original list and create a copy
-        while (originalCurrent != null) {
-            current.next = new ListNode(originalCurrent.val);
-            current = current.next;
-            originalCurrent = originalCurrent.next;
-        }
-
-        return newHead; 
-    }
     
+        public static void main(String[] args) {
+            ListNode l1 = new ListNode(1);
+            l1.next = new ListNode(2);
+            l1.next.next = new ListNode(3);
+    
+            ListNode l2 = new ListNode(4);
+            l2.next = new ListNode(5);
+    
+            // Concatenate the two lists
+            ListNode concat = concatenate(l1, l2);
+    
+            // Print the concatenated list
+            printList(concat);
+        }
+
 }
